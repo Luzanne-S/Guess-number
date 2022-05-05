b26ab2f3c40e1b665edc28d324ddefb124f7e51d
Luzanne <86235334+Luzanne-S@users.noreply.github.com>
2022-05-05 22:17:11 +0200

    Added script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+let secretNumber = Math.round(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  //if the input is null
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+  //if player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+  //guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high' : 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Oh no...You lost the game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+    }
+  }
+});
+//resetting the game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.round(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
